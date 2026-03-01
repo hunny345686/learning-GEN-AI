a190fb46eefdd7f4ce2e2f9f817ee370e5daeb2f
ipremsingh <prem.singh@oodlestechnologies.com>
2026-03-01 14:37:48 +0530

    Learnig AI - initial code start with noral chat app

--- day1/index.js
+++ day1/index.js
@@ -1,4 +1,26 @@
-function sum(a, b) {
-    return a + b;
+import dotenv from 'dotenv';
+dotenv.config();
+import OpenAI from 'openai';
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+
+const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY,
+});
+
+
+async function main() {
+    const response = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        temperature: 0.7,
+        messages: [
+            { role: "system", content: "You are a senior AI architect." },
+            { role: "user", content: "Explain RAG in simple terms." }
+        ],
+    });
+    console.log(response.choices[0].message.content);
 }
+
+main();
+
 
